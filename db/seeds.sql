da44f9c28631a1bc3f00f899fece31c007807376
Juno Brown <amarbrown@gmail.com>
2021-01-26 18:03:10 -0500

    Added all folders and files needed to make the app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Double Double Quadruple Trouble');
+INSERT INTO burger (name) VALUES ('Heart Attack Burger');
+INSERT INTO burger (name, devoured) VALUES ('Triple Dunk Guac Burger', true);
+INSERT INTO burger (name, devoured) VALUES ('Mac Whopper Deepfried', true);
+INSERT INTO burger (name, devoured) VALUES ('Gut Pain', true);
+INSERT INTO burger (name) VALUES ('Everything but the kitchen sink');
