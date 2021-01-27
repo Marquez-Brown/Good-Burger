eabc61f730e8187a26ffdafe06ebd9e27c69e992
Juno Brown <amarbrown@gmail.com>
2021-01-26 23:16:49 -0500

    changed table to plural

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burger (name) VALUES ('Double Double Quadruple Trouble');
-INSERT INTO burger (name) VALUES ('Heart Attack Burger');
-INSERT INTO burger (name, devoured) VALUES ('Triple Dunk Guac Burger', true);
-INSERT INTO burger (name, devoured) VALUES ('Mac Whopper Deepfried', true);
-INSERT INTO burger (name, devoured) VALUES ('Gut Pain', true);
-INSERT INTO burger (name) VALUES ('Everything but the kitchen sink');
+INSERT INTO burgers (name) VALUES ('Double Double Quadruple Trouble');
+INSERT INTO burgers (name) VALUES ('Heart Attack Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Triple Dunk Guac Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mac Whopper Deepfried', true);
+INSERT INTO burgers (name, devoured) VALUES ('Gut Pain', true);
+INSERT INTO burgers (name) VALUES ('Everything but the kitchen sink');
